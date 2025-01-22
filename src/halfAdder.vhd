0021980e86dea205095b6514bc2a4eefb4907dcb
stevooooooo <github noreply stimperley85@gmail.com@users.noreply.github.com>
2025-01-21 17:20:46 -0700

    finished ICE2

--- src/halfAdder.vhd
+++ src/halfAdder.vhd
@@ -1,5 +1,17 @@
 --+----------------------------------------------------------------------------
---| 
+--| --| FILENAME      : halfAdder.vhd
+--| AUTHOR(S)     : Capt Warner
+--| CREATED       : 01/17/2017
+--| DESCRIPTION   : This file implements a one bit half adder.
+--|
+--| DOCUMENTATION : None
+--+----------------------------------------------------------------------------
+--| REQUIRED FILES :
+--|
+--|    Libraries : ieee
+--|    Packages  : std_logic_1164, numeric_std, unisim
+--|    Files     : NONE
+--+----------------------------------------------------------------------------
 --| COPYRIGHT 2017 United States Air Force Academy All rights reserved.
 --| 
 --| United States Air Force Academy     __  _______ ___    _________ 
@@ -18,7 +30,25 @@
 --|
 --|    i_<port name>            = on-chip input port
 --|    o_<port name>            = on-chip output port
+--| 
+--| NAMING CONVENTIONS :
 --|
+--|    xb_<port name>           = off-chip bidirectional port ( _pads file )
+--|    xi_<port name>           = off-chip input port         ( _pads file )
+--|    xo_<port name>           = off-chip output port        ( _pads file )
+--|    b_<port name>            = on-chip bidirectional port
+--|    i_<port name>            = on-chip input port
+--|    o_<port name>            = on-chip output port
+--|    c_<signal name>          = combinatorial signal
+--|    f_<signal name>          = synchronous signal
+--|    ff_<signal name>         = pipeline stage (ff_, fff_, etc.)
+--|    <signal name>_n          = active low signal
+--|    w_<signal name>          = top level wiring signal
+--|    g_<generic name>         = generic
+--|    k_<constant name>        = constant
+--|    v_<variable name>        = variable
+--|    sm_<state machine type>  = state machine type definition
+--|    s_<signal name>          = state name
 --+----------------------------------------------------------------------------
 library ieee;
   use ieee.std_logic_1164.all;
@@ -29,8 +59,9 @@ entity halfAdder is
   port(
 	i_A     : in  std_logic; -- 1-bit input port
 	i_B     : in  std_logic; 
-	o_S     : out std_logic  -- 1-bit output port (NOTE: NO semicolon on LAST port only!)	    
+	o_S     : out std_logic;  -- 1-bit output port (NOTE: NO semicolon on LAST port only!)	    
 	-- TODO:  Carry port
+	o_Cout  : out std_logic
   ); -- the semicolon is here instead
 end halfAdder;
 
@@ -43,5 +74,6 @@ begin
 	-- *concurrent* signal assignments
 	o_S    <= i_A xor i_B;
 	-- TODO:  Carry signal assignment
+	o_Cout <= i_A and i_B;
 	
 end halfAdder_arch;
--- src/halfAdder_tb.vhd
+++ src/halfAdder_tb.vhd
@@ -23,7 +23,11 @@ architecture test_bench of halfAdder_tb is
   -- this must exactly match entity declaration of the thing you are testing
   component halfAdder is
 	port(
+    i_A     : in  std_logic; -- 1-bit input port
+    i_B     : in  std_logic;
+    o_S     : out std_logic;  -- 1-bit output port
 		-- TODO: copy in port map from halfAdder.vhd
+	o_Cout  : out std_logic
 	); -- the semicolon is here instead	
   end component;
 
@@ -31,10 +35,12 @@ architecture test_bench of halfAdder_tb is
   -- declare signals needed to stimulate the UUT inputs
   signal w_sw1 : std_logic := '0';
   -- TODO:  sw0 signal
+  signal w_sw0 : std_logic := '0';
   
   -- also need signals for the outputs of the UUT
   signal w_led1 : std_logic := '0';
   -- TODO:  led0 signal
+  signal w_led0 : std_logic := '0';
 
   
 begin
@@ -44,8 +50,9 @@ begin
 	halfAdder_inst : halfAdder port map (
 		i_A     => w_sw1, -- notice comma (not a semicolon)
 		i_B     => w_sw0,
-		o_S     => w_led0 -- no comma on LAST one
+		o_S     => w_led0, -- no comma on LAST one
 		-- TODO:  map Cout 
+		o_Cout  => w_led1
 	);
 
 
@@ -60,8 +67,16 @@ begin
 		    -- read as "check that w_lec0 = '0' and if it doesn't then give the message 'bad sum' with an error."
             assert w_led0 = '0' report "bad sum" severity error;
             assert w_led1 = '0' report "bad carry" severity error;
+		 w_sw1 <= '0'; w_sw0 <= '1'; wait for 10 ns;
+            assert w_led0 = '1' report "bad sum" severity error;
+            assert w_led1 = '0' report "bad carry" severity error;
+         w_sw1 <= '1'; w_sw0 <= '0'; wait for 10 ns;
+            assert w_led0 = '1' report "bad sum" severity error;
+            assert w_led1 = '0' report "bad carry" severity error;
 		-- TODO:  rest of test plan
-		
+         w_sw1 <= '1'; w_sw0 <= '1'; wait for 10 ns;
+            assert w_led0 = '0' report "bad sum" severity error;
+            assert w_led1 = '1' report "bad carry" severity error;	
 		wait; -- wait forever
 	end process;	
 	-----------------------------------------------------	
